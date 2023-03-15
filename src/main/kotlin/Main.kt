2d99cc977aaba7ec35a26515b7d1a671ebdf463a
winnieSikinyi <winniesikinyi@gmail.com>
2023-03-15 13:02:40 +0300

    changes

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -68,14 +68,14 @@ fun robotDrink(humanAge: Int){
 
 fun digit(){
     for (n in 1..100){
-        if ( n % 3 == 0){
-            println("Fizz")
-            }
-        else if(n % 5 == 0){
-            println("Buzz")
-        }
-        else if (n%3==0 && n%5==0){
+        if ( n%3==0 && n%5==0){
             println("FizzBuzz")
+            }
+        else if(n % 3 == 0){
+            println("Fizz")
+        }
+        else if ( n % 5 == 0){
+            println("Buzz")
 
     }
 
